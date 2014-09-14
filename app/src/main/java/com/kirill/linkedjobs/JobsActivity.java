575e514eed0e5b980b023d26996a32498aadc438
Kirill Zhukov <zhukov.kirill.96@gmail.com>
2014-09-13 23:57:42 -0700

    Overrided onCreate method in JobsActivity

--- app/src/main/java/com/kirill/linkedjobs/JobsActivity.java
+++ app/src/main/java/com/kirill/linkedjobs/JobsActivity.java
@@ -1,10 +1,18 @@
 package com.kirill.linkedjobs;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by kirill on 9/13/14.
  */
-public class JobsActivity {
+public class JobsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
 
 
+    }
 }
