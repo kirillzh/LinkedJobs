800ab507fc1b74f0695eaf3196a2cd6a1a0e461e
Kirill Zhukov <zhukov.kirill.96@gmail.com>
2014-09-18 16:14:56 -0700

    Fixed custom ArrayAdapter. Last posted jobs will be displayed by default without searching.

--- app/src/main/java/com/kirill/linkedjobs/JobsListVewAdapter.java
+++ app/src/main/java/com/kirill/linkedjobs/JobsListVewAdapter.java
@@ -1,5 +1,6 @@
 package com.kirill.linkedjobs;
 
+import android.app.Activity;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,25 +9,29 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 /**
- * Created by kirill on 9/18/14.
- * My GitHub: https://github.com/ZhukovKirill
- */
+* Created by kirill on 9/18/14.
+* My GitHub: https://github.com/ZhukovKirill
+*/
 public class JobsListVewAdapter extends ArrayAdapter<String> {
 
     private final Context context;
+    private final int layoutResourceId;
     private final String[] values;
 
-    public JobsListVewAdapter(Context context, String[] values) {
-        super(context, R.layout.job_listview, values);
+    public JobsListVewAdapter(Context context, int layoutResourceId, String[] values) {
+        super(context, layoutResourceId, values);
+        this.layoutResourceId = layoutResourceId;
         this.context = context;
         this.values = values;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        LayoutInflater inflater = (LayoutInflater) context
-                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View companyView = inflater.inflate(R.layout.job_listview, parent, false);
+
+        View companyView = convertView;
+        LayoutInflater inflater = ((Activity)context).getLayoutInflater();
+        companyView = inflater.inflate(layoutResourceId, parent, false);
+
         TextView textView = (TextView) companyView.findViewById(R.id.company_name);
         textView.setText(values[position]);
         return companyView;
