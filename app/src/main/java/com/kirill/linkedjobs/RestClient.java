1b1b9918c32bb55b40a9329b81e37a2db03c192f
Kirill Zhukov <zhukov.kirill.96@gmail.com>
2014-09-26 00:57:12 -0700

    Moving from Async HTTP library to RetroFit.

--- app/src/main/java/com/kirill/linkedjobs/RestClient.java
+++ app/src/main/java/com/kirill/linkedjobs/RestClient.java
@@ -1,14 +1,15 @@
 package com.kirill.linkedjobs;
 
-import com.loopj.android.http.*;
+import java.util.List;
+
+import retrofit.RestAdapter;
+import retrofit.http.GET;
 
 /**
  * Created by kirill on 9/20/14.
  * My GitHub: https://gthub.com/ZhukovKirill
  */
-public class RestClient {
 
-    private static final String BASE_URL = "http://api.linkedin.com/v1/";
 //    private static final String JOB_SEARCH_URL = BASE_URL + "job-search";
 //    private static final String JOBS_URL = BASE_URL + "jobs";
 //    private static final String PEOPLE_URL = BASE_URL + "people/~";
@@ -17,21 +18,32 @@ public class RestClient {
 //    private static final String QUESTION_MARK = "?";
 //    private static final String EQUALS = "=";
 
-    private static AsyncHttpClient client = new AsyncHttpClient();
+public class RestClient {
+    private static final String BASE_URL = "http://api.linkedin.com/v1";
 
-    public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
-        client.get(url, params, responseHandler);
+    static class Job {
+        String location, companyName;
+        int companyId;
+
     }
+    interface GetJobs {
+        @GET("/job-search?oauth2_access_token=" + Credentials.ACCESS_TOKEN + "&format=json")
+        void getJobs();
 
-    public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
-        client.post(getAbsoluteUrl(url), params, responseHandler);
+        List<Job> jobs();
     }
-
-    private static String getAbsoluteUrl(String relativeUrl) {
-        return BASE_URL + relativeUrl;
+    public static void main(String... args) {
+// Create a very simple REST adapter which points the GitHub API endpoint.
+        RestAdapter restAdapter = new RestAdapter.Builder()
+                .setEndpoint(BASE_URL)
+                .build();
+// Create an instance of our GitHub API interface.
+        GetJobs lnkedin = restAdapter.create(GetJobs.class);
+// Fetch and print a list of the contributors to this library.
+        List<Job> jobs = lnkedin.jobs();
+        for (Job job : jobs) {
+            System.out.println(job.companyName + " (" + job.location + ")");
+        }
     }
-
-
-
-
 }
+
